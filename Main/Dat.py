fe2eba913c8872d387617c6e39af899bf4525bfd
jkfknbchdx <denskillhigh@mail.ru>
2023-03-02 22:37:11 +0500

    qustom_queue push

--- Main/Dat.py
+++ Main/Dat.py
@@ -30,28 +30,28 @@ class Stack:
 
 
 
-stack = Stack()
-stack.push('data1')
-data = stack.pop()
+#stack = Stack()
+#stack.push('data1')
+#data = stack.pop()
 
 # стэк стал пустой
-print(stack.top)
+#print(stack.top)
 # None
 
 # pop() удаляет элемент и возвращает данные удаленного элемента
-print(data)
+#print(data)
 # 'data1'
 
 
-stack = Stack()
-stack.push('data1')
-stack.push('data2')
-data = stack.pop()
+#stack = Stack()
+#stack.push('data1')
+#stack.push('data2')
+#data = stack.pop()
 
 # теперь последний элемента содержит данные data1
-print(stack.top.data)
+#print(stack.top.data)
 # 'data1'
 
 # данные удаленного элемента
-print(data)
+#print(data)
 # 'data2'
