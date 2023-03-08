6e32107376a6c9101789a3ed9b5bafdb5ffdc99c
jkfknbchdx <denskillhigh@mail.ru>
2023-03-09 00:06:41 +0500

    linked list test +

--- tests/linked_list_test.py
+++ tests/linked_list_test.py
@@ -1,27 +1,23 @@
 from unittest import TestCase, main
 from Main.linked_list import LinkedList, Node, ll
 import unittest
+from io import StringIO
+from contextlib import redirect_stdout
 
 
 class Linked_listtest(TestCase):
-    ll = LinkedList()
-    ll.insert_beginning({'id': 1})
-    ll.insert_at_end({'id': 2})
-    ll.insert_at_end({'id': 3})
-    ll.insert_beginning({'id': 0})
-
-
-    def test_insert_beginning(self):
-        # self.assertEqual(ll.insert_beginning({'id': 1}))
-        pass
-
-
-    def test_insert_at_end(self):
-        # self.assertEqual(ll.insert_at_end())
-        pass
 
     def test_print_ll(self):
-        self.assertEqual(ll.print_ll(), None)
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        expected_output = "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+        f = StringIO()
+        with redirect_stdout(f):
+            ll.print_ll()
+        self.assertEqual(f.getvalue().strip(), expected_output)
 
 
 if __name__ == '__main__':
